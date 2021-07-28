f17caddb2ca924c0d75efcc69c45dc0d60bc5bc5
Arpan Sen <warpan2020@gmail.com>
2021-07-28 15:43:35 +0530

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('./db')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const port = process.env.PORT
 
 var postMessageRoutes = require('./controllers/postMessageController')
 
@@ -9,7 +10,7 @@ var postMessageRoutes = require('./controllers/postMessageController')
 var app = express()
 app.use(bodyParser.json())
 app.use(cors({origin:'http://localhost:3000'}))
-app.listen(4000,()=>console.log('Server started at : 4000'))
+app.listen(port,()=>console.log('Server started at : 4000'))
 
 
 app.use('/postMessages',postMessageRoutes)
